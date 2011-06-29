bc1e95afc1c40bc3958567e30e338b9bfe98e2db
pazz <patricktotzke@gmail.com>
2011-06-29 22:32:05 +0100

    killed tags in sort mail

--- sort_mail.py
+++ sort_mail.py
@@ -46,25 +46,24 @@ _list('agda-course@inf.ed.ac.uk', 'agda')
 #############################################
 # do teh monkey
 def tag_message(msg, *tags):
-        msg.freeze()
-        for tag in tags:
-                if tag[0] == '+':
-                        msg.add_tag(tag[1:])
-                elif tag[0] == '-':
-                        msg.remove_tag(tag[1:])
-                else:
-                        msg.add_tag(tag)
-        msg.thaw()
+    msg.freeze()
+    for tag in tags:
+        if tag[0] == '+':
+                msg.add_tag(tag[1:])
+        elif tag[0] == '-':
+                msg.remove_tag(tag[1:])
+        else:
+                msg.add_tag(tag)
+    msg.thaw()
 
 def tag_search(db, search, *tags):
-        q = notmuch.Query(db, search)
-        count = 0
-        for msg in q.search_messages():
-                count += 1
-                tag_message(msg, *tags)
-
-        if count > 0:
-                logging.debug('Tagging %d messages with (%s)' % (count, ' '.join(tags)))
+    q = notmuch.Query(db, search)
+    count = 0
+    for msg in q.search_messages():
+        count += 1
+        tag_message(msg, *tags)
+    if count > 0:
+            logging.debug('Tagging %d messages with (%s)' % (count, ' '.join(tags)))
 
 db = notmuch.Database(mode=notmuch.Database.MODE.READ_WRITE)
 logging.basicConfig(level=LOGLEVEL)
@@ -72,39 +71,23 @@ logging.basicConfig(level=LOGLEVEL)
 q_new = notmuch.Query(db, 'tag:new')
 n_msgs = 0
 for msg in q_new.search_messages():
-        msg.freeze()
-        n_msgs += 1
+    msg.freeze()
+    n_msgs += 1
 
-# Take care of basics
-tag_search(db, 'tag:new', '+unread', '+unseen')
 
 # Tag things
 for filter, tags in _tags:
     tag_search(db, '%s and tag:new' % filter, *tags)
 
 # Ignore things I sent
-tag_search(db, 'tag:new and tag:sent', '-unseen', '-new', '-unread', '+watch')
+tag_search(db, 'tag:new and tag:sent', '-unseen', '-new', '-unread', '-killed')
 
-# Update watch tag
+# Update killtag
 for msg in q_new.search_messages():
-    q = notmuch.Query(db, 'tag:watch and thread:%s' % msg.get_thread_id())
+    q = notmuch.Query(db, 'tag:killed and thread:%s' % msg.get_thread_id())
     if len(list(q.search_messages())) > 0:
-        logging.debug('watching %s' % msg.get_message_id())
-        msg.add_tag('watch')
-
-# Watched items should go to inbox
-tag_search(db, 'tag:new and tag:watch', '+inbox', '-new')
-
-# Ignore threads that I've already seen
-q = notmuch.Query(db, 'tag:new and tag:list')
-for msg in q.search_messages():
-    q2 = notmuch.Query(db, 'thread:%s and not tag:unseen' % msg.get_thread_id())
-    if len(list(q2.search_messages())) > 0:
-        msg.remove_tag('unseen')
-        msg.remove_tag('new')
-
-# Remove new from sorted list items
-tag_search(db, 'tag:new and tag:list', '-new')
+        logging.debug('killing %s' % msg.get_message_id())
+        tag_message(msg, 'killed', '-new')
 
 # Tag remaining new items for inbox
 tag_search(db, 'tag:new', '+inbox', '-new')
@@ -118,9 +101,9 @@ logging.info('Sorted %d messages in %1.2f seconds' % (n_msgs, end_time - start_t
 pic = '/usr/share/icons/Human/scalable/emblems/emblem-mail.svg'
 topic = '\"new mail\"'
 
-q = notmuch.Query(db, 'tag:inbox AND NOT tag:killed')
-count = q.count_messages()
-if count:
-    msg = '\"there are currently %d messages in your inbox\"'%count
-    notify_cmd="/usr/bin/notify-send -t 5 -i %s %s %s"%(pic, topic, msg)
-    os.system(notify_cmd)
+#q = notmuch.Query(db, 'tag:inbox AND NOT tag:killed')
+#count = q.count_messages()
+#if count:
+#    msg = '\"there are currently %d messages in your inbox\"'%count
+#    notify_cmd="/usr/bin/notify-send -t 5 -i %s %s %s"%(pic, topic, msg)
+#    os.system(notify_cmd)
